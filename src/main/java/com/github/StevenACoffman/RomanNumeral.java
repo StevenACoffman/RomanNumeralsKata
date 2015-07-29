16a910080eb3fde24726258668e627f49bf274e7
Steve Coffman <gears@umich.edu>
2015-07-28 22:32:29 -0400

    Ok, refactored until it's pretty. Still passes tests.

--- src/main/java/com/github/StevenACoffman/RomanNumeral.java
+++ src/main/java/com/github/StevenACoffman/RomanNumeral.java
@@ -5,30 +5,15 @@ public class RomanNumeral {
     public static String calculate(int input) {
         String result = "";
         
-        while (input >= 10) {
-            result += "X";
-            input -= 10;
-        }
-        
-        while (input >= 5) {
-            result += "V";
-            input -= 5;
-        }
-        while (input >= 4) {
-            result += "IV";
-            input -= 4;
-        }
-        while (input >= 1) {
-            result += "I";
-            input -= 1;
+        for (RomanNumeralCharacter romanNumeralCharacter : RomanNumeralCharacter.values()) {
+            while (input >= romanNumeralCharacter.getNumericValue()) {
+                result += romanNumeralCharacter.toString();
+                input -= romanNumeralCharacter.getNumericValue();
+            }
         }
         
         return result;
         
-    }
-    
-    private static String repeat(String repeated, int count) {
-        return new String(new char[count]).replace("\0", repeated);
     }
     
 }
---
+++ src/main/java/com/github/StevenACoffman/RomanNumeralCharacter.java
@@ -0,0 +1,28 @@
+package com.github.StevenACoffman;
+
+public enum RomanNumeralCharacter {
+  
+  //@formatter:off
+    X("X", 10),
+    V("V", 5),
+    IV("IV", 4),
+    I("I", 1);
+  //@formatter:on
+    
+    private String character;
+    private int value;
+    
+    private RomanNumeralCharacter(String character, int value) {
+        this.character = character;
+        this.value = value;
+        
+    }
+    
+    public int getNumericValue() {
+        return this.value;
+    }
+    
+    public String toString() {
+        return this.character;
+    }
+}
