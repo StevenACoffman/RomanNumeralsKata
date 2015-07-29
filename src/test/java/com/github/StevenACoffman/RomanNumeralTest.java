b77aee6cf5d75154d5eb41a57031fb9b47ea5bdb
Steve Coffman <gears@umich.edu>
2015-07-29 12:09:02 -0400

    Sort the test assertions

--- src/test/java/com/github/StevenACoffman/RomanNumeralTest.java
+++ src/test/java/com/github/StevenACoffman/RomanNumeralTest.java
@@ -9,27 +9,25 @@ import org.junit.Test;
 public class RomanNumeralTest {
     @Test
     public void convertFromNumberToRomanNumeral() {
-        // Given
-        // When
         // Then
         assertThat(RomanNumeral.calculate(1), is(equalTo("I")));
         assertThat(RomanNumeral.calculate(2), is(equalTo("II")));
         assertThat(RomanNumeral.calculate(4), is(equalTo("IV")));
         assertThat(RomanNumeral.calculate(5), is(equalTo("V")));
         assertThat(RomanNumeral.calculate(6), is(equalTo("VI")));
+        assertThat(RomanNumeral.calculate(9), is(equalTo("IX")));
         assertThat(RomanNumeral.calculate(10), is(equalTo("X")));
         assertThat(RomanNumeral.calculate(20), is(equalTo("XX")));
         assertThat(RomanNumeral.calculate(40), is(equalTo("XL")));
         assertThat(RomanNumeral.calculate(50), is(equalTo("L")));
         assertThat(RomanNumeral.calculate(100), is(equalTo("C")));
+        assertThat(RomanNumeral.calculate(207), is(equalTo("CCVII")));
         assertThat(RomanNumeral.calculate(400), is(equalTo("CD")));
         assertThat(RomanNumeral.calculate(500), is(equalTo("D")));
         assertThat(RomanNumeral.calculate(1000), is(equalTo("M")));
-        assertThat(RomanNumeral.calculate(9), is(equalTo("IX")));
-        assertThat(RomanNumeral.calculate(207), is(equalTo("CCVII")));
+        
         assertThat(RomanNumeral.calculate(1954), is(equalTo("MCMLIV")));
         assertThat(RomanNumeral.calculate(1990), is(equalTo("MCMXC")));
-        
         assertThat(RomanNumeral.calculate(2015), is(equalTo("MMXV")));
         assertThat(RomanNumeral.calculate(3999), is(equalTo("MMMCMXCIX")));
         
